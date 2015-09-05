8d1059cd817cf45386c7fba9efc7636e4d0dd636
kartavec <jenia@webkontrol.ru>
2015-09-05 15:08:13 +0300

    Создали класс Point

---
+++ Snake/Snake/Point.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+	class Point
+	{
+		public int x;
+		public int y;
+		public char sym;
+
+		public void Draw()
+		{
+			Console.SetCursorPosition( x, y );
+			Console.Write( sym );			
+		}
+	}
+}
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,25 +10,20 @@ namespace Snake
 	{
 		static void Main( string[] args )
 		{
-			int x1 = 1;
-			int y1 = 3;
-			char sym1 = '*';
+			Point p1 = new Point();
+			p1.x = 1;
+			p1.y = 3;
+			p1.sym = '*';
+			p1.Draw();
 
-			Draw( x1, y1, sym1 );
+			Point p2 = new Point();
+			p2.x = 4;
+			p2.y = 5;
+			p2.sym = '#';
 
-			int x2 = 4;
-			int y2 = 5;
-			char sym2 = '#';
-
-			Draw( x2, y2, sym2 );
+			p2.Draw();
 
 			Console.ReadLine();
-		}
-
-		static void Draw(int x, int y, char sym)
-		{
-			Console.SetCursorPosition( x, y );
-			Console.Write( sym );
 		}
 	}
 }
