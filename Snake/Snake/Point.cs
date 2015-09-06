5d0008da5a9385d1aa93a0b4bd9e46823f963aab
kartavec <jenia@webkontrol.ru>
2015-09-06 20:50:44 +0300

    Создаем конструкторы класса Point

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -12,6 +12,17 @@ namespace Snake
 		public int y;
 		public char sym;
 
+		public Point()
+		{
+		}
+
+		public Point(int _x, int _y, char _sym)
+		{
+			x = _x;
+			y = _y;
+			sym = _sym;
+		}
+
 		public void Draw()
 		{
 			Console.SetCursorPosition( x, y );
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,17 +10,10 @@ namespace Snake
 	{
 		static void Main( string[] args )
 		{
-			Point p1 = new Point();
-			p1.x = 1;
-			p1.y = 3;
-			p1.sym = '*';
+			Point p1 = new Point(1, 3, '*');
 			p1.Draw();
 
-			Point p2 = new Point();
-			p2.x = 4;
-			p2.y = 5;
-			p2.sym = '#';
-
+			Point p2 = new Point(4, 5, '#');
 			p2.Draw();
 
 			Console.ReadLine();
