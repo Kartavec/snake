b69a32d6751096fe09d4c2479eb3bc3d56c8d68d
jenia <jenia@webkontorl.ru>
2015-09-07 17:31:36 +0300

    Эксперементируем с фнукциями

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,13 +10,44 @@ namespace Snake
 	{
 		static void Main( string[] args )
 		{
-			Point p1 = new Point(1, 3, '*');
-			p1.Draw();
+			int x = 1;
+			IncFailed( x );
+			Console.WriteLine( "Call IncFailed. x = " + x );
 
-			Point p2 = new Point(4, 5, '#');
-			p2.Draw();
+			x = 1;
+			x = Inc( x );
+			Console.WriteLine( "Call Inc. x = " + x );
 
-			Console.ReadLine();
+			Point p1 = new Point( 1, 3, '*');
+			Move( p1, 10, 10 );
+			Console.WriteLine( "Call Move. p1.x = " + p1.x + ", p1.y = " + p1.y );
+
+			Update(p1);
+			Console.WriteLine( "Call Move. p1.x = " + p1.x + ", p1.y = " + p1.y );
+
 		}
+
+		public static void IncFailed( int value )
+		{
+			value = value + 1;
+		}
+
+		public static int Inc( int value )
+		{
+			return value + 1;
+		}
+
+		public static void Move( Point p, int dx, int dy )
+		{
+			p.x = p.x + dx;
+			p.y = p.y + dy;
+		}
+
+		public static void Update( Point p )
+		{
+			p = new Point();
+		}
+
+
 	}
 }
