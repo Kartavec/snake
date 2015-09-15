dcebcbdebc19a8a1e3afb745cd52cf6b997d3b7f
jenia <jenia@webkontorl.ru>
2015-09-15 18:44:54 +0300

    Write text "Game over"

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -47,7 +47,30 @@ namespace Snake
 					snake.HandleKey( key.Key );
 				}
 			}
+			WriteGameOver();
+			Console.ReadLine();
       }
+
+
+		static void WriteGameOver()
+		{
+			int xOffset = 25;
+			int yOffset = 8;
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.SetCursorPosition( xOffset, yOffset++ );
+			WriteText( "============================", xOffset, yOffset++ );
+			WriteText( "И Г Р А    О К О Н Ч Е Н А", xOffset + 1, yOffset++ );
+			yOffset++;
+			WriteText( "Автор: Евгений Картавец", xOffset + 2, yOffset++ );
+			WriteText( "Специально для GeekBrains", xOffset + 1, yOffset++ );
+			WriteText( "============================", xOffset, yOffset++ );
+		}
+
+		static void WriteText( String text, int xOffset, int yOffset )
+		{
+			Console.SetCursorPosition( xOffset, yOffset );
+			Console.WriteLine( text );
+		}
 
 	}
 }
